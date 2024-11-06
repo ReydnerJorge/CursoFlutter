acfa535ae52a0e7d054b5e5e07d27b3af2d9891b
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2024-11-06 19:34:54 -0300

    Update for dart

---
+++ dart/fundamentos/tipos_basicos_1.dart
@@ -0,0 +1,22 @@
+/* 
+- Numeros (int e double)
+- String (String)
+- Booleano (bool)
+*/
+
+void main() {
+  int n1 = 3;
+
+  double n2 = -5.67.abs();
+
+  double n3 = double.parse("12.765");
+
+  num n4 = 6;
+
+  print(n1.abs() + n2 + n3 + n4);
+
+  n4 = 6.7;
+
+  
+  print(n1.abs() + n2 + n3 + n4);
+}
